9b23e9b8eb97f1e51f8c33385381930101c4b535
Nolan <nolan.hellyer@gmail.com>
2018-12-01 10:55:03 -0800

    all functionality present

--- public/js/render.js
+++ public/js/render.js
@@ -6,45 +6,54 @@ const server = "https://polar-inlet-56421.herokuapp.com";
 // Drops in each blog post
 function fillBlogContent(data) {
   data.reverse().forEach(post => {
-    // Generate the elements for displaying each post
-    const postElement = document.createElement("div");
-    const titleElement = document.createElement("h2");
-    const contentElement = document.createElement("p");
-    const hrElement = document.createElement("hr");
-    const editElement = document.createElement("a");
-    const deleteElement = document.createElement("a");
-
-    // Configure the post element
-    postElement.classList.add("element-post");
-
-    // Configure the title element
-    titleElement.innerText = post.title;
-
-    // Configure the content element
-    contentElement.innerText = post.content;
-
-    // Configure the edit button
-    editElement.innerText = "edit";
-    editElement.id = "edit";
-    editElement.classList.add("element-button");
-    editElement.setAttribute("data_id", post.id);
-
-    // Configure the delete button
-    deleteElement.innerText = "delete";
-    deleteElement.id = "delete";
-    deleteElement.classList.add("element-button");
-    deleteElement.setAttribute("data_id", post.id);
-
-    // Attach the elements to the post element
-    postElement.appendChild(titleElement);
-    postElement.appendChild(contentElement);
-    postElement.appendChild(editElement);
-    postElement.appendChild(deleteElement);
-    postElement.appendChild(hrElement);
-
-    // Attach post element to the DOM
-    blogContent.appendChild(postElement);
+    blogContent.appendChild(buildPost(post));
   });
+}
+
+function buildPost(post) {
+  // Generate the elements for displaying each post
+  const postElement = document.createElement("div");
+  const titleElement = document.createElement("h2");
+  const contentElement = document.createElement("p");
+  const hrElement = document.createElement("hr");
+  const editElement = document.createElement("a");
+  const deleteElement = document.createElement("a");
+
+  // Configure the post element
+  postElement.classList.add("element-post");
+
+  // Configure the title element
+  titleElement.innerText = post.title;
+  titleElement.classList.add("title");
+
+  // Configure the content element
+  contentElement.innerText = post.content;
+  contentElement.classList.add("content");
+
+  // Configure the edit button
+  editElement.innerText = "edit";
+  editElement.id = "edit";
+  editElement.classList.add("element-button");
+  editElement.setAttribute("data-id", post.id);
+  editElement.addEventListener("click", editHandler);
+
+  // Configure the delete button
+  deleteElement.innerText = "delete";
+  deleteElement.id = "delete";
+  deleteElement.classList.add("element-button");
+  deleteElement.setAttribute("data-id", post.id);
+  deleteElement.addEventListener("click", deleteHandler);
+
+  // Attach the elements to the post element
+  postElement.appendChild(titleElement);
+  postElement.appendChild(contentElement);
+  postElement.appendChild(editElement);
+  postElement.appendChild(deleteElement);
+  postElement.appendChild(hrElement);
+
+  // Attach post element to the DOM
+
+  return postElement;
 }
 
 // Add labels to an array of unlabelled input elements
@@ -89,6 +98,7 @@ function renderForm (method, action, id, post) {
   // Configure title input
   titleInput.setAttribute("name", "title");
   titleInput.setAttribute("type", "text");
+  titleInput.id = "titleHTML";
   if (post) {
     titleInput.innerText = post.title;
   }
@@ -105,6 +115,7 @@ function renderForm (method, action, id, post) {
   closeButton.setAttribute("type", "button");
   closeButton.setAttribute("name", "close");
   closeButton.innerText = "X Close";
+  closeButton.addEventListener("click", closeHandler);
 
   // Configure submit button
   submitButton.setAttribute("type", "submit");
@@ -126,16 +137,88 @@ function renderForm (method, action, id, post) {
   // Remove New Post button
   newPost.style.display = "none";
 
-  console.log(form);
-
   // Add to the DOM
   formArea.appendChild(form);
 }
 
+function makeEditable(element) {
+  const title = element.querySelector(".title");
+  const content = element.querySelector(".content");
+  const editButton = element.querySelector("#edit");
+  const deleteButton = element.querySelector("#delete");
+
+  const titleInput = document.createElement("input");
+  const contentInput = document.createElement("textarea");
+  const cancelButton = document.createElement("a");
+  const submitButton = document.createElement("a");
+
+  const titleValue = title.innerText;
+  const contentValue = content.innerText;
+  const postId = editButton.getAttribute("data-id");
+
+  cancelButton.innerText = "cancel";
+  cancelButton.id = "cancel";
+  cancelButton.classList.add("element-button");
+  cancelButton.setAttribute("data-id", postId);
+  cancelButton.addEventListener("click", (e) => {
+    makeNotEditable(e.target);
+  });
+
+  submitButton.innerText = "submit";
+  submitButton.id = "submit";
+  submitButton.classList.add("element-button");
+  submitButton.setAttribute("data-id", postId);
+  submitButton.addEventListener("click", submitEditHandler);
+
+  // console.log(titleValue, contentValue);
+
+  titleInput.setAttribute("name", "title");
+  titleInput.setAttribute("type", "text");
+  // titleInput.id = "titleHTML";
+
+  contentInput.setAttribute("name", "content");
+  contentInput.setAttribute("rows", "8");
+  contentInput.setAttribute("cols", "80");
+
+  titleInput.value = titleValue;
+  contentInput.value = contentValue;
+
+  element.insertBefore(titleInput, title);
+  title.remove();
+
+  element.insertBefore(contentInput, content);
+  content.remove();
+
+  element.insertBefore(cancelButton, editButton);
+  editButton.remove();
+
+  element.insertBefore(submitButton, deleteButton);
+  deleteButton.remove();
+}
+
+function makeNotEditable(element) {
+  console.log(element);
+  const postId = element.getAttribute("data-id");
+  axios.get(server + "/posts/" + postId)
+  .then(res => {
+    const newElement = buildPost(res.data.data);
+    const post = element.parentElement;
+
+    blogContent.insertBefore(newElement, post);
+    post.remove();
+  })
+  .catch(err => {
+    console.log(err);
+  })
+}
+
+
 // Render the success callout
-function successMessage() {
+function successMessage(response) {
   const messageArea = document.getElementById("message-area");
   const successCallout = document.createElement("div");
+
+  blogContent.insertBefore(buildPost(response.data.data), blogContent.children[0]);
 
   successCallout.classList.add("callout");
   successCallout.classList.add("success");
@@ -143,9 +226,11 @@ function successMessage() {
   successCallout.innerHTML = "<p>You did it! I was worried for a moment...</p>";
 
   messageArea.appendChild(successCallout);
+
+  setTimeout(() => successCallout.remove(), 3000);
 }
 
-function errorMessage() {
+function errorMessage(error) {
   const messageArea = document.getElementById("message-area");
   const alertCallout = document.createElement("div");
 
@@ -153,6 +238,8 @@ function errorMessage() {
   alertCallout.classList.add("alert");
 
   alertCallout.innerHTML = "<p>You done goofed.</p>";
+
+  console.log(error);
 
   messageArea.appendChild(alertCallout);
 }
@@ -165,26 +252,71 @@ function newPostHandler(e) {
 }
 
 function postHandler(e) {
+  e.preventDefault();
+
   const formArea = document.getElementById("form-area");
   const newPost = document.getElementById("new-post");
 
-  e.preventDefault();
+
+  title =   e.target
+             .parentElement
+             .querySelector("input[name=\'title\']")
+             .value;
+
+  content = e.target
+             .parentElement
+             .querySelector("textarea[name=\'content\']")
+             .value;
 
   formArea.innerHTML = "";
   newPost.style.display = "flex";
 
-  axios.post(server + "/posts")
-  .then(res => successMessage())
-  .catch(err => errorMessage());
+  axios.post(server + "/posts", { title, content })
+  .then(res => successMessage(res))
+  .catch(err => errorMessage(err));
 }
 
 function editHandler(e) {
-  const id = e.target.getAttribute("data_id");
+  const id = e.target.getAttribute("data-id");
 
+  makeEditable(e.target.parentElement);
+}
+
+function submitEditHandler(e) {
+  const id = e.target.getAttribute("data-id");
+  console.log(e.target.parentElement.querySelector("input[name=\'title\']"))
+  const title = e.target.parentElement.querySelector("input[name=\'title\']").value;
+  const content = e.target.parentElement.querySelector("textarea[name=\'content\']").value;
+
+  axios.put(server + "/posts/" + id, { title, content })
+  .then(res => {
+    const newElement = buildPost(res.data.data);
+    const post = e.target.parentElement;
+
+    blogContent.insertBefore(newElement, post);
+    post.remove();
+  })
 }
 
 function deleteHandler(e) {
+  axios.delete(server + "/posts/" + e.target.getAttribute("data-id"))
+    .then(res => {
+      e.target.parentElement.remove();
+    })
+    .catch(err => errorMessage(err));
+}
 
+function cancelEditHandler(e) {
+
+  makeNotEditable(e.target.parentElement);
+}
+
+function closeHandler(e) {
+  const newPost = document.getElementById("new-post");
+  const formArea = document.getElementById("form-area");
+
+  formArea.innerHTML = "";
+  newPost.style.display = "flex";
 }
 
 module.exports = { fillBlogContent, newPostHandler };
